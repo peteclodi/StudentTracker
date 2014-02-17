23d265a9ec77071675a4021a08a614f5218d0bfa
Pete <pete.clodi@gmail.com>
2014-02-16 23:49:53 -0600

    Started creating the PHP API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,13 +27,21 @@ studentTrackerApp.provider('students', function(){
     var studentArray = [];
 
     this.init = function(grades) {
-        studentArray = [
-            {id: 0, firstName: "Jimmy", lastName: "", grade: grades[0]},
-            {id: 1, firstName: "Timmy", lastName: "", grade: grades[4]},
-            {id: 2, firstName: "Janey", lastName: "", grade: grades[6]},
-            {id: 3, firstName: "Debbie", lastName: "", grade: grades[1]},
-            {id: 4, firstName: "Ralphie", lastName: "", grade: grades[3]}
-        ];
+        $.ajax({
+            url: "http://localhost:8080/php/students",
+            type: "GET"
+        }).done(function(data, status, jqXHR){
+            studentArray = data;
+        }).fail(function(jqXHR, status, error){
+            studentArray = [];
+        });
+//        studentArray = [
+//            {id: 0, firstName: "Jimmy", lastName: "", grade: grades[0]},
+//            {id: 1, firstName: "Timmy", lastName: "", grade: grades[4]},
+//            {id: 2, firstName: "Janey", lastName: "", grade: grades[6]},
+//            {id: 3, firstName: "Debbie", lastName: "", grade: grades[1]},
+//            {id: 4, firstName: "Ralphie", lastName: "", grade: grades[3]}
+//        ];
     };
 
     this.$get = function() {
--- assets/php/init.php
+++ assets/php/init.php
@@ -1,77 +1,122 @@
 <?php
-    $con = mysqli_connect("localhost","dev","dev","student_tracker");
+    function init_grades(){
+        $con = mysqli_connect("localhost","dev","dev","student_tracker");
 
-    // Check connection
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit();
+        }
+
+        echo("Seeding grade_levels table");
+
+        $query = "INSERT INTO grade_levels (name) VALUES (?)";
+        $stmt = mysqli_prepare($con, $query);
+
+        $gradeName = 'pre-K';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Kindergarten';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'First';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Second';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Third';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Fourth';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Fifth';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Sixth';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Seventh';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        $gradeName = 'Eighth';
+        mysqli_stmt_bind_param($stmt,"s", $gradeName);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$gradeName' to MySQL\n";
+        }
+
+        mysqli_stmt_close($stmt);
+        mysqli_close($con);
     }
 
-    echo("Seeding grade_levels table");
+    function init_test_students() {
+        $query = "INSERT INTO students (first_name, last_name, grade) VALUES (?, '', ?)";
+        $stmt = mysqli_prepare($con, $query);
 
-    $query = "INSERT INTO grade_levels (name) VALUES (?)";
-    $stmt = mysqli_prepare($con, $query);
+        $firstName = 'Jimmy';
+        $grade = 0;
+        mysqli_stmt_bind_param($stmt,"si", $firstName, $grade);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$firstName' to MySQL\n";
+        }
 
-    $gradeName = 'pre-K';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
+        $firstName = 'Timmy';
+        $grade = 5;
+        mysqli_stmt_bind_param($stmt,"si", $firstName, $grade);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$firstName' to MySQL\n";
+        }
+
+        $firstName = 'Katie';
+        $grade = 3;
+        mysqli_stmt_bind_param($stmt,"si", $firstName, $grade);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$firstName' to MySQL\n";
+        }
+
+        $firstName = 'Ralphie';
+        $grade = 4;
+        mysqli_stmt_bind_param($stmt,"si", $firstName, $grade);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$firstName' to MySQL\n";
+        }
+
+        $firstName = 'Bobby';
+        $grade = 1;
+        mysqli_stmt_bind_param($stmt,"si", $firstName, $grade);
+        if(!mysqli_stmt_execute($stmt)) {
+            echo "Failed to INSERT '$firstName' to MySQL\n";
+        }
+
+        mysqli_stmt_close($stmt);
+        mysqli_close($con);
     }
-
-    $gradeName = 'Kindergarten';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'First';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Second';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Third';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Fourth';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Fifth';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Sixth';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Seventh';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    $gradeName = 'Eighth';
-    mysqli_stmt_bind_param($stmt,"s", $gradeName);
-    if(!mysqli_stmt_execute($stmt)) {
-        echo "Failed to INSERT '$gradeName' to MySQL\n";
-    }
-
-    mysqli_stmt_close($stmt);
-    mysqli_close($con);
 ?>
