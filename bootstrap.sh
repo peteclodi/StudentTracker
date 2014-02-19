aaf80dbafb845eedbbc827dfb87662168164e84a
peteclodi <pete.clodi@gmail.com>
2014-02-19 17:04:15 -0600

    stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Update packages"
 apt-get update
-
-echo "Install Apache2"
 apt-get install -y apache2
-
-echo "Map the assets folder to the Apache2 web root directory"
 rm -rf /var/www
-ln -fs /vagrant/assets /var/www
+ln -fs /vagrant/assets/api /var/www
